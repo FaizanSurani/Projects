f162789716fdcdf7c60bf422ff5f5a740e312cc1
Faizan-Surani <suranifaizan52@gmail.com>
2024-03-09 22:55:43 +0530

    Create Routing for the pages and Change the Header component

--- Food-Delivery-App/client-side/src/App.jsx
+++ Food-Delivery-App/client-side/src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Orders from "./pages/Orders";
 
 export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </Router>
   );
 }
--- Food-Delivery-App/client-side/src/components/Header.jsx
+++ Food-Delivery-App/client-side/src/components/Header.jsx
@@ -1,37 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import src from "../assets/Food.png";
 
 export default function Header() {
   return (
     <>
-      <nav className="bg-white shadow-lg">
+      <nav className="bg-green-500 shadow-lg">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 text-black">
           <div className="relative flex h-16 items-center justify-between">
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  <a
-                    href="#"
-                    className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                    aria-current="page">
-                    Dashboard
-                  </a>
-                  <a
-                    href="#"
-                    className="text-black hover:bg-black hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                    Team
-                  </a>
-                  <a
-                    href="#"
-                    className="text-black hover:bg-black hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                    Projects
-                  </a>
-                  <a
-                    href="#"
-                    className="text-black hover:bg-black hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                    Calendar
-                  </a>
-                </div>
+              <div className="max-h-10 max-w-12 border-2 border-green-400 rounded-full mr-10">
+                <Link to="/">
+                  <img src={src} alt="..." />
+                </Link>
               </div>
+              <ul className="flex space-x-5">
+                <li className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="text-black hover:bg-black hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                  <Link to="/orders">Orders</Link>
+                </li>
+                <li className="text-black hover:bg-black hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                  <Link to="/sign-in">Sign In</Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
