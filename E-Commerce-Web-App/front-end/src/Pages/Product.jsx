597e80553c2d6cc4347b1fea8063b77da254b85b
Faizan-Surani <suranifaizan52@gmail.com>
2024-04-25 23:35:05 +0530

    Add the Product page functionality

--- E-Commerce-Web-App/front-end/src/Pages/Product.jsx
+++ E-Commerce-Web-App/front-end/src/Pages/Product.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay";
 
 export default function Product() {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => {
+    return e.id === Number(productId);
+  });
+
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 }
