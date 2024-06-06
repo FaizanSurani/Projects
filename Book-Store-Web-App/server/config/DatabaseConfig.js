b3bff30bbb65373925f108342b81eb2d0076b46f
Faizan-Surani <suranifaizan52@gmail.com>
2024-06-06 16:51:34 +0530

    Add the UserSchema and Orders Schema in Project

--- Book-Store-Web-App/server/config/DatabaseConfig.js
+++ Book-Store-Web-App/server/config/DatabaseConfig.js
@@ -4,10 +4,16 @@ require("dotenv").config();
 
 const DBConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGOURI);
+    await mongoose.connect(`${process.env.MONGOURI}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connection Established Succesfully!");
   } catch (error) {
     console.log(error);
   }
 };
 
 DBConnect();
+
+module.exports = DBConnect();
---
+++ Book-Store-Web-App/server/models/UserSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true["Enter Your Name!"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true["Enter Your Email!"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true["Enter Your Password!"],
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    address: {
+      type: String,
+      required: true["Enter Your Address!"],
+    },
+    avatar: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/128/3177/3177440.png",
+    },
+    favourites: [{ type: mongoose.Types.ObjectId, ref: "books" }],
+    cart: [{ type: mongoose.Types.ObjectId, ref: "books" }],
+    orders: [{ type: mongoose.Types.ObjectId, ref: "orders" }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", User);
