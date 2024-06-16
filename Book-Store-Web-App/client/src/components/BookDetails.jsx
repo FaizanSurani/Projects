33d32ca8d04c24df797f6b02e14352649c6036f1
Faizan-Surani <suranifaizan52@gmail.com>
2024-06-16 13:38:54 +0530

    Add the favoruites and Add to cart buttons, Add isLoggedIn functionality

--- Book-Store-Web-App/client/src/components/BookDetails.jsx
+++ Book-Store-Web-App/client/src/components/BookDetails.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Loader from "./Loader";
 import { GrLanguage } from "react-icons/gr";
 import axios from "axios";
+import { FaHeart, FaShoppingCart } from "react-icons/fa";
+import { AuthContext } from "./AuthContext";
 
 export default function BookDetails() {
   const { id } = useParams();
-  console.log(id);
+  const { isLoggedIn } = useContext(AuthContext);
+  const { role } = useContext(AuthContext);
   const [bookDetails, setBookDetails] = useState("");
 
   useEffect(() => {
@@ -20,13 +23,25 @@ export default function BookDetails() {
   return (
     <>
       {bookDetails && (
-        <div className="px-8 md:px-12 py-8 bg-zinc-900 flex md:flex-row flex-col gap-8">
-          <div className="bg-zinc-800 rounded p-4 h-[60vh] lg:h-[88vh] lg:w-3/6 w-full flex justify-center items-center">
-            <img
-              className="lg:h-[70vh] h-[50vh] rounded"
-              src={bookDetails.url}
-              alt="/"
-            />
+        <div className="px-4 md:px-12 py-8 bg-zinc-900 flex lg:flex-row flex-col gap-8 items-start">
+          <div className="lg:w-3/6 w-full">
+            <div className="lg:flex-row flex-col bg-zinc-800 rounded p-12 flex justify-around">
+              <img
+                className="lg:h-[60vh] h-[50vh] rounded"
+                src={bookDetails.url}
+                alt="/"
+              />
+              {isLoggedIn === true && (
+                <div className="flex flex-row lg:flex-col items-center justify-between lg:justify-start lg:mt-0 :mt-8">
+                  <button className="bg-white rounded-full text-3xl p-3 text-red-500">
+                    <FaHeart />
+                  </button>
+                  <button className="bg-white rounded lg:rounded-full text-4xl lg:text-3xl p-3 mt-0 lg:mt-8 text-blue-500 flex items-center justify-center">
+                    <FaShoppingCart />
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
           <div className="p-4 lg:w-3/6 w-full">
             <h1 className="text-4xl text-zinc-300 font-semibold">
