44685ff98e704242cd719b010bc0733410ee8ffb
Faizan-Surani <suranifaizan52@gmail.com>
2024-08-08 16:00:07 +0530

    Handle add hotel form

---
+++ Hotel-Booking-Web-App/client/src/components/Facilities.jsx
@@ -0,0 +1,10 @@
+export const facilites = [
+  "Free Wifi",
+  "Parking",
+  "Airport Shuffle",
+  "Family Rooms",
+  "Non-Smoking Rooms",
+  "Outdoor Pool",
+  "Spa",
+  "Fitness Center",
+];
--- Hotel-Booking-Web-App/client/src/pages/AddHotel.jsx
+++ Hotel-Booking-Web-App/client/src/pages/AddHotel.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { hotelTypes } from "../components/HotelTypes";
+import { facilites } from "../components/Facilities";
 
 const AddHotel = () => {
   const handleChange = (e) => {};
   const handleSubmit = (e) => {};
+
   return (
     <>
       <div className="min-h-screen px-12 py-8 flex justify-center items-center">
@@ -66,7 +69,7 @@ const AddHotel = () => {
             <label htmlFor="rating">Select an Rating</label>
             <select
               name="rating"
-              className="border rounded w-full p-2 bg-blue-200 font-normal">
+              className="border rounded w-full p-2 bg-blue-200 font-normal mt-2">
               <option value="" className="text-sm font-semibold">
                 Select an Rating
               </option>
@@ -76,14 +79,55 @@ const AddHotel = () => {
             </select>
           </div>
           <div className="mt-4">
-            <label htmlFor="">Hotel Type</label>
-            <input
-              type="text"
-              name="type"
-              className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
-              required
-              onChange={handleChange}
-            />
+            <label htmlFor="type">Hotel Type</label>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-white gap-2 mt-2">
+              {hotelTypes.map((type) => (
+                <label
+                  htmlFor="radio"
+                  className="cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold active:bg-blue-500">
+                  <input type="radio" value={type} className="" />
+                  <span>{type}</span>
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className="mt-4">
+            <label htmlFor="type">Facilities</label>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-white gap-1 mt-2">
+              {facilites.map((type) => (
+                <label
+                  htmlFor="checkbox"
+                  className="flex text-md gap-1 font-bold">
+                  <input type="checkbox" value={type} />
+                  {type}
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className="mt-4 bg-blue-400 rounded">
+            <label htmlFor="">Guests</label>
+            <div className="flex justify-around text-white">
+              <div>
+                <label htmlFor="city">Adult</label>
+                <input
+                  type="text"
+                  name="guests"
+                  className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="guests">Child</label>
+                <input
+                  type="text"
+                  name="guests"
+                  className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
           </div>
           <div className="mt-4">
             <button className=" text-white w-full px-7 py-3 rounded bg-red-500 hover:bg-red-600 uppercase transition duration-150 ease-in-out">
