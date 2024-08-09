e5f59e09b6883cfac60b85e246a9846161fd036e
Faizan-Surani <suranifaizan52@gmail.com>
2024-08-09 16:17:50 +0530

    Complete the form of add hotel

--- Hotel-Booking-Web-App/client/src/pages/AddHotel.jsx
+++ Hotel-Booking-Web-App/client/src/pages/AddHotel.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { hotelTypes } from "../components/HotelTypes";
 import { facilites } from "../components/Facilities";
+import axios from "axios";
 
 const AddHotel = () => {
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const [formData, setFormData] = useState({
+    name: "",
+    city: "",
+    country: "",
+    description: "",
+    price: "",
+    rating: "",
+    type: "",
+    facilities: [],
+    guests: {
+      adult: "",
+      child: "",
+    },
+    images: [],
+  });
+
+  const {
+    name,
+    city,
+    country,
+    description,
+    price,
+    rating,
+    type,
+    facilities,
+    guests: { adult, child },
+    images,
+  } = formData;
+
+  const handleChange = (e) => {
+    const { name, type, value } = e.target;
+
+    if (name === "adult" || name === "child") {
+      setFormData((prevState) => ({
+        ...prevState,
+        guests: {
+          ...prevState.guests,
+          [name.split(",")[1]]: value,
+        },
+      }));
+    } else if (name === "checkbox") {
+      const checkedFacilites = facilities.includes(value)
+        ? facilities.filter((facility) => facility !== value)
+        : [...facilities, value];
+      setFormData({ ...formData, facilities: checkedFacilites });
+    } else if (name === "radio") {
+      setFormData({ ...formData, type: value });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setFormData({ ...formData, images: files });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // const response = await axios.post("");
+  };
 
   return (
     <>
@@ -17,6 +77,7 @@ const AddHotel = () => {
             <label>Hotel Name</label>
             <input
               type="text"
+              value={name}
               className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
               required
               name="name"
@@ -28,6 +89,7 @@ const AddHotel = () => {
               <label htmlFor="city">Hotel City</label>
               <input
                 type="text"
+                value={city}
                 name="city"
                 className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
                 required
@@ -39,6 +101,7 @@ const AddHotel = () => {
               <input
                 type="text"
                 name="country"
+                value={country}
                 className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
                 required
                 onChange={handleChange}
@@ -50,6 +113,7 @@ const AddHotel = () => {
             <textarea
               rows={5}
               name="description"
+              value={description}
               className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
               required
               onChange={handleChange}
@@ -59,6 +123,7 @@ const AddHotel = () => {
             <label htmlFor="price">Price Per Night</label>
             <input
               type="number"
+              value={price}
               name="price"
               className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
               required
@@ -69,6 +134,7 @@ const AddHotel = () => {
             <label htmlFor="rating">Select an Rating</label>
             <select
               name="rating"
+              value={rating}
               className="border rounded w-full p-2 bg-blue-200 font-normal mt-2">
               <option value="" className="text-sm font-semibold">
                 Select an Rating
@@ -81,12 +147,17 @@ const AddHotel = () => {
           <div className="mt-4">
             <label htmlFor="type">Hotel Type</label>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-white gap-2 mt-2">
-              {hotelTypes.map((type) => (
+              {hotelTypes.map((types) => (
                 <label
                   htmlFor="radio"
                   className="cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold active:bg-blue-500">
-                  <input type="radio" value={type} className="" />
-                  <span>{type}</span>
+                  <input
+                    type="radio"
+                    name="type"
+                    checked={types === type}
+                    value={type}
+                  />
+                  <span>{types}</span>
                 </label>
               ))}
             </div>
@@ -94,25 +165,27 @@ const AddHotel = () => {
           <div className="mt-4">
             <label htmlFor="type">Facilities</label>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-white gap-1 mt-2">
-              {facilites.map((type) => (
+              {facilites.map((facility) => (
                 <label
+                  key={facility}
                   htmlFor="checkbox"
-                  className="flex text-md gap-1 font-bold">
-                  <input type="checkbox" value={type} />
-                  {type}
+                  className="flex text-sm gap-2 font-bold ">
+                  <input type="checkbox" value={facility} />
+                  {facility}
                 </label>
               ))}
             </div>
           </div>
-          <div className="mt-4 bg-blue-400 rounded">
+          <div className="mt-4 bg-blue-400 rounded p-2">
             <label htmlFor="">Guests</label>
             <div className="flex justify-around text-white">
               <div>
                 <label htmlFor="city">Adult</label>
                 <input
                   type="text"
-                  name="guests"
-                  className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
+                  value={adult}
+                  name="adult"
+                  className="w-full mb-2 p-2 outline-none bg-blue-200 rounded"
                   required
                   onChange={handleChange}
                 />
@@ -121,8 +194,9 @@ const AddHotel = () => {
                 <label htmlFor="guests">Child</label>
                 <input
                   type="text"
-                  name="guests"
-                  className="w-full mt-2 p-2 outline-none bg-blue-200 rounded"
+                  value={child}
+                  name="child"
+                  className="w-full mb-2 p-2 outline-none bg-blue-200 rounded"
                   required
                   onChange={handleChange}
                 />
@@ -130,7 +204,19 @@ const AddHotel = () => {
             </div>
           </div>
           <div className="mt-4">
-            <button className=" text-white w-full px-7 py-3 rounded bg-red-500 hover:bg-red-600 uppercase transition duration-150 ease-in-out">
+            <label htmlFor="price">Images</label>
+            <div className="border rounded p-4 flex flex-col gap-4 mt-2">
+              <input
+                type="file"
+                multiple
+                accept="image/*"
+                value={images}
+                className="w-full text-white font-normal"
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <button className="text-white w-full px-7 py-3 rounded bg-red-500 hover:bg-red-600 uppercase transition duration-150 ease-in-out">
               Add hotel
             </button>
           </div>
