ffc7b32690f4649b628adab2ee51ad906e6f829c
Faizan-Surani <suranifaizan52@gmail.com>
2024-09-17 16:18:10 +0530

    Working on Search Result cards

---
+++ Hotel-Booking-Web-App/client/src/components/SearchResultCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchResultCard = () => {
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-[2fr_3fr] border border-slate-300 rounded-lg p-5 gap-5">
+      <div className="w-full h-[200px]">
+        <img
+          src=""
+          alt=""
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+      <div className="flex flex-col justify-between">Default Columns</div>
+    </div>
+  );
+};
+
+export default SearchResultCard;
--- Hotel-Booking-Web-App/client/src/pages/Search.jsx
+++ Hotel-Booking-Web-App/client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { SearchContext } from "../context/SearchContext";
 import { useQuery } from "@tanstack/react-query";
+import SearchResultCard from "../components/SearchResultCard";
 
 const Search = () => {
   const search = useContext(SearchContext);
@@ -22,7 +23,22 @@ const Search = () => {
 
   return (
     <>
-      <h1>Search Results</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5 min-h-screen">
+        <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+          <div className="space-y-5">
+            <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+              Filter By:
+            </h3>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div className="flex justify-center items-center">
+            <span className="text-xl font-bold">
+              <SearchResultCard />
+            </span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
