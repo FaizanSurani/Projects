de4d47ed51f5389afa14922edeb24864d506b62f
Faizan-Surani <suranifaizan52@gmail.com>
2024-09-25 15:45:41 +0530

    Completed the search backend

--- Hotel-Booking-Web-App/server/src/routes/hotelSearch.js
+++ Hotel-Booking-Web-App/server/src/routes/hotelSearch.js
@@ -3,26 +3,36 @@ const hotel = require("../models/hotelSchema");
 
 router.get("/searchHotel", async (req, res) => {
   try {
+    const query = constructSearchQuery(req.query);
+
+    let sortOptions = {};
+    switch (req.query.sortOptions) {
+      case "ratings":
+        sortOptions = { ratings: -1 };
+        break;
+
+      case "pricePerNightAsc":
+        sortOptions = { pricePerNight: 1 };
+        break;
+
+      case "pricePerNigthDesc":
+        sortOptions = { pricePerNight: -1 };
+        break;
+    }
+
     const pageSize = 5;
     const pageNumber = parseInt(
       req.query.page ? req.query.page.toString() : "1"
     );
+
     const skip = (pageNumber - 1) * pageSize;
 
-    const query = {};
+    const hotels = await hotel
+      .find(query)
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(pageSize);
 
-    if (req.query.destination) {
-      query.hotelCity = req.query.destination;
-    }
-
-    if (req.query.adultCount) {
-      query.adultCount = req.query.adultCount;
-    }
-    if (req.query.childCount) {
-      query.childCount = req.query.childCount;
-    }
-
-    const hotels = await hotel.find(query).skip(skip).limit(pageSize);
     const total = await hotel.countDocuments(query);
 
     const response = {
@@ -40,5 +50,59 @@ router.get("/searchHotel", async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
+const constructSearchQuery = (queryParams) => {
+  const constructQuery = {};
+
+  if (queryParams.destination) {
+    constructQuery.$or = [
+      { hotelCity: new RegExp(queryParams.destination, "i") },
+      { hotelCountry: new RegExp(queryParams.destination, "i") },
+    ];
+  }
+
+  if (queryParams.adultCount) {
+    constructQuery.adultCount = {
+      $gte: parseInt(queryParams.adultCount),
+    };
+  }
+
+  if (queryParams.childCount) {
+    constructQuery.childCount = {
+      $gte: parseInt(queryParams.childCount),
+    };
+  }
+
+  if (queryParams.facilities) {
+    constructQuery.facilities = {
+      $all: Array.isArray(queryParams.facilities)
+        ? queryParams.facilities
+        : [queryParams.facilities],
+    };
+  }
+
+  if (queryParams.hotelType) {
+    constructQuery.hotelType = {
+      $in: Array.isArray(queryParams.hotelType)
+        ? queryParams.hotelType
+        : [queryParams.hotelType],
+    };
+  }
+
+  if (queryParams.rating) {
+    const ratings = Array.isArray(queryParams.rating)
+      ? queryParams.rating.map((star) => parseInt(star))
+      : parseInt(queryParams.rating);
+    constructQuery.ratings = { $eq: ratings };
+  }
+
+  if (queryParams.pricePerNight) {
+    constructQuery.pricePerNight = {
+      $lte: parseInt(queryParams.pricePerNight).toString(),
+    };
+  }
+
+  return constructQuery;
+};
 
 module.exports = router;
